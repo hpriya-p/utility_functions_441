51cd22c56e0ffd3f49f9780e801ac624fab9fec2
hpriya-p <haripriya.pulyassary@gmail.com>
2019-11-17 13:06:19 -0500

    bugs fixed

--- evaluate.py
+++ evaluate.py
@@ -2,17 +2,21 @@ import pandas as pd
 import numpy as np
 from util import create_bagged_predictor, create_train_model_fn
 
-def evaluate_bagged_model(train_x, train_y, test_x, test_y, model, model_param, p, verbose=False, seed=35901):
+def class_prob_avg_fn(class_probabilities):
+    K = len(class_probabilities[0])
+    return [np.mean([x[i] for x in class_probabilities]) for i in range(K)]
+def evaluate_bagged_model(train_x, train_y, test_x, test_y, model, model_param, p, prob_aggr_fn=class_prob_avg_fn, verbose=False, seed=35901):
     train_model_fn = create_train_model_fn(model, model_param)
     predictor_fn = create_bagged_predictor(train_x, train_y, train_model_fn, p, verbose, seed)
 
-    test_data = test_x
+    test_data = pd.concat([test_x, test_y], axis=1)
     response_var = test_y.name
     features = [x for x in test_data.columns if x != response_var]
     classes = np.unique(train_y.values)
     n_t = test_data.shape[0]
-    predictions = [np.argmax(predictor_fn(test_data.iloc[i])) for i in range(n_t)]
+    predicted_probs = [predictor_fn(test_data.iloc[i], prob_aggr_fn) for i in range(n_t)]
+    predictions = [np.argmax(predicted_probs[i]) for i in range(n_t)]
     num_correct = sum([1 if classes[predictions[i]] == test_data[response_var].iloc[i] else 0 for i in range(n_t)])
-    return num_correct, n_t, predictions
+    return num_correct, n_t, predicted_probs, predictions
     
     
--- util.py
+++ util.py
@@ -19,9 +19,9 @@ def create_bagged_predictor(train_x, train_y, train_model_fn, p, verbose=False, 
     p = p                       # number of features per bootstrap sample
 
     # Preliminaries
-    train_data = train_x
+    train_data = pd.concat([train_x, train_y], axis=1)
     response_var = train_y.name
-    features = [x for x in train_data if x != response_var]
+    features = [x for x in train_data.columns if x != response_var]
     if(verbose): print("features: " + ", ".join(features))
 
     # Create Bootstrap Samples
@@ -31,21 +31,17 @@ def create_bagged_predictor(train_x, train_y, train_model_fn, p, verbose=False, 
     # Train models on bootstrapped data/feature sets
     Models = []
     for i in range(B):
-        data = boot_data_sets[i]
+        data = train_data.loc[boot_data_sets[i]]
         feats = boot_feat_sets[i]
         Models.append(train_model_fn(data[feats], data[response_var])) 
 
     # Create and return predictor function which bags these models
-    def predictor_fn(X):
+    def predictor_fn(X, prob_aggr_fn = np.mean):
         probabilities = []
         for i in range(B):
-            try:
-                probabilities.append(Models[i].predict_proba(X))
-            except:
-                print("ERROR: could not predict model")
-                return -1
+            probabilities.append(list(Models[i].predict_proba(X[boot_feat_sets[i]].values.reshape(1, -1))[0]))
 
-        return np.mean(probabilities)
+        return prob_aggr_fn(probabilities)
 
     return predictor_fn
 
@@ -58,7 +54,7 @@ def create_train_model_fn(model_obj, params_dict):
     """
 
     def train_model_fn(X, y):
-        model_obj.set_params(params_dict)
+        model_obj.set_params(**params_dict)
         model_obj.fit(X, y)
         return model_obj
     return train_model_fn
