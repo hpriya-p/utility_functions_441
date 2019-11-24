bb46725a927d10800f2693698ee81e0083e590e6
hpriya-p <haripriya.pulyassary@gmail.com>
2019-11-24 13:52:06 -0500

    fixed out of bag error calculation (NOTE: SIGNIFICANT CHANGES IN THIS VERSION)

--- evaluate.py
+++ evaluate.py
@@ -1,30 +1,71 @@
 import pandas as pd
 import numpy as np
-from util import create_bagged_predictor, create_train_model_fn
-
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import metrics
+from util import create_bagged_predictor
+from copy import deepcopy
+"""
 def class_prob_avg_fn(class_probabilities):
     K = len(class_probabilities[0])
     return [np.mean([x[i] for x in class_probabilities]) for i in range(K)]
 
 def class_majority_vote(class_probabilities):
-    print(class_probabilities)
     K = len(class_probabilities[0])
     votes = [np.argmax(pr) for pr in class_probabilities]
     num_votes_per_class = [len([i for i in votes if i == v]) for v in range(K)]
     return [1 if i == np.argmax(num_votes_per_class) else 0 for i in range(K)]
+"""
 
-def evaluate_bagged_model(train_x, train_y, test_x, test_y, model, model_param, p, prob_aggr_fn=class_prob_avg_fn, verbose=False, seed=35901):
-    train_model_fn = create_train_model_fn(model, model_param)
-    predictor_fn, models = create_bagged_predictor(train_x, train_y, train_model_fn, p, verbose, seed)
+def calculate_oob(data_x, data_y, model, model_param, num_feats_per_bootstrap='all', seed=35901):
+    if(num_feats_per_bootstrap=='all'):
+        num_feats_per_bootstrap = data_x.shape[1]
+    predictor_fn, models, not_seen, seen = create_bagged_predictor(data_x, data_y, model, model_param, num_feats_per_bootstrap, seed)
+    B = len(models)
+   
+    train_errs = []
+    test_errs = []
+    for i in range(B):
+        test_data = not_seen[i]
+        m = models[i]
+        m.fit(data_x.loc[seen[i]], data_y.loc[seen[i]])
+        train_err = metrics.accuracy_score(m.predict(data_x.loc[seen[i]]), data_y.loc[seen[i]])
+        train_errs.append(train_err)
+        test_err = metrics.accuracy_score(models[i].predict(data_x.loc[test_data]), data_y.loc[test_data])
+        test_errs.append(test_err)
+    
+    unseen_points_dict = dict()
+    for point in data_x.index:
+        lst = [i for i in range(B) if point in not_seen[i]]
+        if(len(lst) > 0):
+            unseen_points_dict[point] = deepcopy(lst)
 
-    test_data = pd.concat([test_x, test_y], axis=1)
-    response_var = test_y.name
-    features = [x for x in test_data.columns if x != response_var]
-    classes = np.unique(train_y.values)
-    n_t = test_data.shape[0]
-    predicted_probs = [predictor_fn(test_data.iloc[i], prob_aggr_fn) for i in range(n_t)]
-    predictions = [classes[np.argmax(predicted_probs[i])] for i in range(n_t)]
-    num_correct = sum([1 if predictions[i] == test_data[response_var].iloc[i] else 0 for i in range(n_t)])
-    return num_correct, n_t, predicted_probs, predictions, models
-    
-    
+    n_correct = 0.0
+    n_total = 0
+    for pt, model_inds in unseen_points_dict.items():
+        predicted_val = predictor_fn(data_x.loc[pt].values.reshape(1, -1),  model_inds)
+        n_total += 1
+        if(predicted_val == data_y.loc[pt]):
+            n_correct += 1
+
+    test_oob = n_correct/n_total
+
+
+    seen_points_dict = dict()
+    for point in data_x.index:
+        lst = [i for i in range(B) if point in np.unique(seen[i])]
+        if(len(lst) > 0):
+            seen_points_dict[point] = deepcopy(lst)
+
+    n_correct = 0.0
+    n_total = 0
+    for pt, model_inds in seen_points_dict.items():
+        predicted_val = predictor_fn(data_x.loc[pt].values.reshape(1, -1), model_inds)
+        n_total += 1
+        if(predicted_val == data_y.loc[pt]):
+            n_correct += 1
+
+    train_oob = n_correct/n_total
+    return {'test oob':test_oob, 'train oob': train_oob, 'train errors': train_errs, 'test errors': test_errs, 'predictor': predictor_fn}
+
+
+
--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import random as rand
 import numpy as np
+from copy import deepcopy
 
-def create_bagged_predictor(train_x, train_y, train_model_fn, p, verbose=False, seed=35901):
+def create_bagged_predictor(train_x, train_y, model, model_params, p, seed=35901):
     """
     Returns predictor_fn(X), a bagged predictor function. Requires the following inputs:
         * train_x, train_y: pandas DataFrames
@@ -15,7 +16,6 @@ def create_bagged_predictor(train_x, train_y, train_model_fn, p, verbose=False, 
    
     # CONSTANTS
     n = train_x.shape[0]        # sample size
-    print(n)
     B = 50                      # number of bootstrap samples
     p = p                       # number of features per bootstrap sample
 
@@ -23,43 +23,50 @@ def create_bagged_predictor(train_x, train_y, train_model_fn, p, verbose=False, 
     train_data = pd.concat([train_x, train_y], axis=1)
     response_var = train_y.name
     features = [x for x in train_data.columns if x != response_var]
-    if(verbose): print("features: " + ", ".join(features))
+
 
     # Create Bootstrap Samples
 
     randomly_sampled_data = rand.choices(train_data.index, k=n*B)
     boot_data_sets = [randomly_sampled_data[(i-1)*n:i*n] for i in range(1,B+1)] #setting this to k=2*n improves performance
-    print(len(boot_data_sets))
     boot_feat_sets = [rand.sample(features, p) for __ in range(B)]
 
     # Train models on bootstrapped data/feature sets
     Models = []
+    not_seen = []
+    seen_data = []
     for i in range(B):
+        model_obj = deepcopy(model)
+        model_obj.set_params(**model_params)
         data = train_data.loc[boot_data_sets[i]]
+        seen_data.append(boot_data_sets[i])
+        unique_vals = np.unique(boot_data_sets[i])
+        not_seen.append([x for x in train_data.index if x not in unique_vals])
         feats = boot_feat_sets[i]
-        Models.append(train_model_fn(data[feats], data[response_var])) 
+        model_obj.fit(train_x.loc[boot_data_sets[i]], train_y.loc[boot_data_sets[i]] )
+        Models.append(model_obj) 
 
     # Create and return predictor function which bags these models
-    def predictor_fn(X, prob_aggr_fn = np.mean):
-        probabilities = []
-        for i in range(B):
-            probabilities.append(list(Models[i].predict_proba(X[boot_feat_sets[i]].values.reshape(1, -1))[0]))
+    def predictor_fn(X, models_to_consider='all'):
+        if(models_to_consider == 'all'):
+            models_to_consider = range(B)
 
-        return prob_aggr_fn(probabilities)
-
-    return predictor_fn, Models
+        votes = [Models[i].predict(X) for i in models_to_consider]
+        return mode(votes)
 
 
+    return predictor_fn, Models, not_seen, seen_data
 
-def create_train_model_fn(model_obj, params_dict):
-    """
-    Given a Scikit-learn model object and a dictionary of parameters + values,
-    returns a train_model_fn which can be provided as input to create_bagged_predictor
-    """
 
-    def train_model_fn(X, y):
-        model_obj.set_params(**params_dict)
-        model_obj.fit(X, y)
-        return model_obj
-    return train_model_fn
+
+def mode(lst_of_votes):
+    #tie breaking: smaller class index
+    frequencies = dict()
+    for v in lst_of_votes:
+        v = v[0]
+        if(v not in frequencies.keys()):
+            frequencies[v] = 0
+        else:
+            frequencies[v] += 1
+    return max(frequencies, key=lambda key: frequencies[key])
 
